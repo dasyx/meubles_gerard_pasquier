c9cfebf0aac8a5a8e0fb923f544b672643e697ed
unknown <seanpnn00@gmail.com>
2023-01-29 23:16:01 +0100

    lowres logo menu js display

--- dist/index.c072103e.js
+++ dist/index.c072103e.js
@@ -541,11 +541,11 @@ var _button = require("../components/button");
 var _buttonDefault = parcelHelpers.interopDefault(_button);
 const myButton = document.getElementById("fb-btn");
 myButton.appendChild((0, _buttonDefault.default)());
-// Affichage conditionnel du header menu dropdown
-var hdBtn = document.getElementById("header_btn");
-var testElement = document.getElementById("test");
+// Affichage conditionnel du menu dropdown header
+let hdBtn = document.getElementById("header_btn");
+let testElement = document.getElementById("test");
 if (window.innerWidth || document.body.clientWidth < 768) {
-    var isVisible = false;
+    let isVisible = false;
     hdBtn.addEventListener("click", function() {
         if (isVisible) {
             testElement.style.visibility = "hidden";
@@ -556,14 +556,54 @@ if (window.innerWidth || document.body.clientWidth < 768) {
         }
     });
 } else testElement.style.visibility = "visible";
-// Affichage conditionnel du header texte en low res
+// Affichage conditionnel du menu dropdown logo lowres
+let lowResButton = document.getElementById("nav_logo-btn");
+let lowResMenu = document.getElementById("nav_logo-lowres");
 if (window.innerWidth || document.body.clientWidth < 768) {
+    let isVisible1 = false;
+    lowResButton.addEventListener("click", function() {
+        if (isVisible1) {
+            lowResMenu.style.display = "none";
+            isVisible1 = false;
+        } else {
+            lowResMenu.style.display = "block";
+            isVisible1 = true;
+        }
+    });
+} else lowResMenu.style.display = "block";
+// Affichage conditionnel du header texte en low res
+if (window.innerWidth || document.body.clientWidth <= 768) {
     document.getElementById("contact").innerHTML = "Contact";
     document.getElementById("follow").innerHTML = "Suivre";
+//document.getElementById("nav_logo-btn").classList.add("menu-burger");
 } else {
     document.getElementById("contact").innerHTML = "Contactez-nous";
     document.getElementById("follow").innerHTML = "Suivez-nous";
+//document.getElementById("nav_logo-btn").classList.remove("menu-burger");
 }
+// Récupérer le bouton
+const button = document.getElementById("nav_logo-btn");
+const hiResMenu = document.getElementById("nav_logo");
+// Définir la résolution d'affichage à laquelle le bouton doit apparaître/disparaître
+const displayWidth = 768;
+// Ajouter un écouteur d'événements pour le redimensionnement de la fenêtre
+window.addEventListener("resize", function() {
+    // Vérifier la largeur de la fenêtre
+    if ((window.innerWidth || document.body.clientWidth) <= displayWidth) {
+        // Afficher le bouton
+        button.style.display = "block";
+        hiResMenu.style.display = "none";
+    } else {
+        // Cacher le bouton
+        button.style.display = "none";
+        hiResMenu.style.display = "";
+    }
+});
+// Vérifier la largeur de la fenêtre au chargement de la page
+if (window.innerWidth <= displayWidth) {
+    button.style.display = "block";
+    document.getElementById("nav_logo").style.display = "none";
+} else button.style.display = "none";
 // Script affichant la date actuelle
 let date = document.getElementById("getDate");
 let actualDate = new Date().getFullYear();
--- src/js/script.js
+++ src/js/script.js
@@ -5,14 +5,18 @@ import "flowbite";
 
 import createButton from "../components/button";
 
+
+
 const myButton = document.getElementById("fb-btn");
 myButton.appendChild(createButton());
 
-// Affichage conditionnel du header menu dropdown
-var hdBtn = document.getElementById("header_btn");
-var testElement = document.getElementById("test");
+
+
+// Affichage conditionnel du menu dropdown header
+let hdBtn = document.getElementById("header_btn");
+let testElement = document.getElementById("test");
 if (window.innerWidth || document.body.clientWidth < 768) {
-  var isVisible = false;
+  let isVisible = false;
   hdBtn.addEventListener("click", function () {
     if (isVisible) {
       testElement.style.visibility = "hidden";
@@ -26,13 +30,70 @@ if (window.innerWidth || document.body.clientWidth < 768) {
   testElement.style.visibility = "visible";
 }
 
-// Affichage conditionnel du header texte en low res
+
+// Affichage conditionnel du menu dropdown logo lowres
+let lowResButton = document.getElementById("nav_logo-btn");
+let lowResMenu = document.getElementById("nav_logo-lowres");
+
 if (window.innerWidth || document.body.clientWidth < 768) {
+  let isVisible = false;
+  lowResButton.addEventListener("click", function () {
+    if (isVisible) {
+      lowResMenu.style.display = "none";
+      isVisible = false;
+    } else {
+      lowResMenu.style.display = "block";
+      isVisible = true;
+    }
+  });
+} else {
+  lowResMenu.style.display = "block";
+}
+
+
+// Affichage conditionnel du header texte en low res
+if (window.innerWidth || document.body.clientWidth <= 768) {
   document.getElementById("contact").innerHTML = "Contact";
   document.getElementById("follow").innerHTML = "Suivre";
+  //document.getElementById("nav_logo-btn").classList.add("menu-burger");
 } else {
   document.getElementById("contact").innerHTML = "Contactez-nous";
   document.getElementById("follow").innerHTML = "Suivez-nous";
+  //document.getElementById("nav_logo-btn").classList.remove("menu-burger");
+}
+
+
+
+// Récupérer le bouton
+const button = document.getElementById("nav_logo-btn");
+const hiResMenu = document.getElementById("nav_logo");
+
+// Définir la résolution d'affichage à laquelle le bouton doit apparaître/disparaître
+const displayWidth = 768;
+
+// Ajouter un écouteur d'événements pour le redimensionnement de la fenêtre
+window.addEventListener("resize", function () {
+  // Vérifier la largeur de la fenêtre
+  if ((window.innerWidth || document.body.clientWidth) <= displayWidth) {
+    // Afficher le bouton
+    button.style.display = "block";
+    hiResMenu.style.display = "none";
+
+  } else {
+    // Cacher le bouton
+    button.style.display = "none";
+    hiResMenu.style.display = "";
+  }
+});
+
+
+
+// Vérifier la largeur de la fenêtre au chargement de la page
+if (window.innerWidth <= displayWidth) {
+  button.style.display = "block";
+  document.getElementById("nav_logo").style.display = "none";
+} else {
+  button.style.display = "none";
 }
 
 
