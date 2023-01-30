824184de5197c4e23d6bd33fb8fc84a519526191
unknown <seanpnn00@gmail.com>
2023-01-30 18:47:01 +0100

    add header dropdown effect + work on nav logo button effects

--- dist/index.c072103e.js
+++ dist/index.c072103e.js
@@ -592,19 +592,7 @@ window.addEventListener("resize", function() {
         hdDropdown.style.display = "none";
     }
 });
-// Vérifier la largeur de la fenêtre au chargement de la page
-/* if (window.innerWidth <= displayWidth) {
-  navLogoBtnLowres.style.display = "flex";
-  navLogoMenuHiRes.style.display = "none";
-  hdBtnLowres.style.display = "block";
-  hdDropdown.style.display = "block"
-
-} else {
-  navLogoBtnLowres.style.display = "none";
-  navLogoMenuHiRes.style.display = "flex";
-  hdBtnLowres.style.display = "none";
-  hdDropdown.style.display = "none";
-} */ // Affichage conditionnel du header texte en low res
+// Affichage conditionnel du header texte en low res
 if (window.innerWidth || document.body.clientWidth <= 768) {
     document.getElementById("contact").innerHTML = "Contact";
     document.getElementById("follow").innerHTML = "Suivre";
@@ -619,11 +607,18 @@ let date = document.getElementById("getDate");
 let actualDate = new Date().getFullYear();
 date.innerHTML = actualDate;
 // Script qui modifie le DOM pour ajouter les liens du header logo lowres
-var ourStory = document.getElementById("our-story");
-var dropdownMenu = document.getElementById("dropdown-menu");
+let ourStory = document.getElementById("histoire");
+let products = document.getElementById("produits");
+let inspirations = document.getElementById("inspirations");
+let surmesure = document.getElementById("surmesure");
+let dropdownMenu1 = document.getElementById("dropdown-menu1");
+let dropdownMenu2 = document.getElementById("dropdown-menu2");
+let dropdownMenu3 = document.getElementById("dropdown-menu3");
+let dropdownMenu4 = document.getElementById("dropdown-menu4");
+// Pour la rubrique notre histoire
 ourStory.addEventListener("click", function() {
-    if (dropdownMenu.classList.contains("hidden")) {
-        dropdownMenu.classList.remove("hidden");
+    if (dropdownMenu1.classList.contains("hidden")) {
+        dropdownMenu1.classList.remove("hidden");
         var listItem1 = document.createElement("li");
         var listItem2 = document.createElement("li");
         var listItem3 = document.createElement("li");
@@ -642,12 +637,102 @@ ourStory.addEventListener("click", function() {
         listItem1.appendChild(link1);
         listItem2.appendChild(link2);
         listItem3.appendChild(link3);
-        dropdownMenu.appendChild(listItem1);
-        dropdownMenu.appendChild(listItem2);
-        dropdownMenu.appendChild(listItem3);
+        dropdownMenu1.appendChild(listItem1);
+        dropdownMenu1.appendChild(listItem2);
+        dropdownMenu1.appendChild(listItem3);
     } else {
-        dropdownMenu.classList.add("hidden");
-        while(dropdownMenu.firstChild)dropdownMenu.removeChild(dropdownMenu.firstChild);
+        dropdownMenu1.classList.add("hidden");
+        while(dropdownMenu1.firstChild)dropdownMenu1.removeChild(dropdownMenu1.firstChild);
+    }
+});
+// Pour la rubrique produits
+products.addEventListener("click", function() {
+    if (dropdownMenu2.classList.contains("hidden")) {
+        dropdownMenu2.classList.remove("hidden");
+        var listItem1 = document.createElement("li");
+        var listItem2 = document.createElement("li");
+        var listItem3 = document.createElement("li");
+        var link1 = document.createElement("a");
+        var link2 = document.createElement("a");
+        var link3 = document.createElement("a");
+        link1.classList.add("rounded-t", "bg-gray-200", "hover:bg-gray-400", "py-2", "px-4", "block", "whitespace-nowrap");
+        link1.setAttribute("href", "#");
+        link1.textContent = "A d\xe9finir";
+        link2.classList.add("bg-gray-200", "hover:bg-gray-400", "py-2", "px-4", "block", "whitespace-nowrap");
+        link2.setAttribute("href", "#");
+        link2.textContent = "A d\xe9finir";
+        link3.classList.add("rounded-b", "bg-gray-200", "hover:bg-gray-400", "py-2", "px-4", "block", "whitespace-nowrap");
+        link3.setAttribute("href", "#");
+        link3.textContent = "A d\xe9finir";
+        listItem1.appendChild(link1);
+        listItem2.appendChild(link2);
+        listItem3.appendChild(link3);
+        dropdownMenu2.appendChild(listItem1);
+        dropdownMenu2.appendChild(listItem2);
+        dropdownMenu2.appendChild(listItem3);
+    } else {
+        dropdownMenu2.classList.add("hidden");
+        while(dropdownMenu2.firstChild)dropdownMenu2.removeChild(dropdownMenu2.firstChild);
+    }
+});
+// Pour la rubrique inspirations
+inspirations.addEventListener("click", function() {
+    if (dropdownMenu3.classList.contains("hidden")) {
+        dropdownMenu3.classList.remove("hidden");
+        var listItem1 = document.createElement("li");
+        var listItem2 = document.createElement("li");
+        var listItem3 = document.createElement("li");
+        var link1 = document.createElement("a");
+        var link2 = document.createElement("a");
+        var link3 = document.createElement("a");
+        link1.classList.add("rounded-t", "bg-gray-200", "hover:bg-gray-400", "py-2", "px-4", "block", "whitespace-nowrap");
+        link1.setAttribute("href", "#");
+        link1.textContent = "A d\xe9finir";
+        link2.classList.add("bg-gray-200", "hover:bg-gray-400", "py-2", "px-4", "block", "whitespace-nowrap");
+        link2.setAttribute("href", "#");
+        link2.textContent = "A d\xe9finir";
+        link3.classList.add("rounded-b", "bg-gray-200", "hover:bg-gray-400", "py-2", "px-4", "block", "whitespace-nowrap");
+        link3.setAttribute("href", "#");
+        link3.textContent = "A d\xe9finir";
+        listItem1.appendChild(link1);
+        listItem2.appendChild(link2);
+        listItem3.appendChild(link3);
+        dropdownMenu3.appendChild(listItem1);
+        dropdownMenu3.appendChild(listItem2);
+        dropdownMenu3.appendChild(listItem3);
+    } else {
+        dropdownMenu3.classList.add("hidden");
+        while(dropdownMenu3.firstChild)dropdownMenu3.removeChild(dropdownMenu3.firstChild);
+    }
+});
+// Pour la rubrique sur-mesure
+surmesure.addEventListener("click", function() {
+    if (dropdownMenu4.classList.contains("hidden")) {
+        dropdownMenu4.classList.remove("hidden");
+        var listItem1 = document.createElement("li");
+        var listItem2 = document.createElement("li");
+        var listItem3 = document.createElement("li");
+        var link1 = document.createElement("a");
+        var link2 = document.createElement("a");
+        var link3 = document.createElement("a");
+        link1.classList.add("rounded-t", "bg-gray-200", "hover:bg-gray-400", "py-2", "px-4", "block", "whitespace-nowrap");
+        link1.setAttribute("href", "#");
+        link1.textContent = "A d\xe9finir";
+        link2.classList.add("bg-gray-200", "hover:bg-gray-400", "py-2", "px-4", "block", "whitespace-nowrap");
+        link2.setAttribute("href", "#");
+        link2.textContent = "A d\xe9finir";
+        link3.classList.add("rounded-b", "bg-gray-200", "hover:bg-gray-400", "py-2", "px-4", "block", "whitespace-nowrap");
+        link3.setAttribute("href", "#");
+        link3.textContent = "A d\xe9finir";
+        listItem1.appendChild(link1);
+        listItem2.appendChild(link2);
+        listItem3.appendChild(link3);
+        dropdownMenu4.appendChild(listItem1);
+        dropdownMenu4.appendChild(listItem2);
+        dropdownMenu4.appendChild(listItem3);
+    } else {
+        dropdownMenu4.classList.add("hidden");
+        while(dropdownMenu4.firstChild)dropdownMenu4.removeChild(dropdownMenu4.firstChild);
     }
 });
 
