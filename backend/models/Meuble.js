0f47ded61a85b5852b94fff2be28471517cbf6db
unknown <seanpnn00@gmail.com>
2023-02-06 14:26:33 +0100

    fix meubles model

---
+++ backend/models/Meuble.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const MeubleSchema = new mongoose.Schema({
+  name: {
+    required: true,
+    type: String,
+  },
+  type: {
+    required: true,
+    type: String,
+  },
+  inspiration : {
+    required: true,
+    type: String,
+  },
+  description: {
+    required: true,
+    type: String,
+  },
+  imgUrl: {
+    required: true,
+    type: String,
+  },
+});
+
+const Meuble = mongoose.model("Meuble", MeubleSchema);
+
+module.exports = Meuble;
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const classiqueMeuble = require('../models/classiqueMeuble');
+const Meuble = require('../models/Meuble');
 const router = express.Router();
 
-//Post Method
-router.post('/post', async (req, res) => {
-    const data = new classiqueMeuble({
+//Post Meubles Method
+router.post('/postMeuble', async (req, res) => {
+    const data = new Meuble({
         name: req.body.name,
+        type: req.body.type,
+        inspiration: req.body.inspiration,
         description: req.body.description,
         imgUrl: req.body.imgUrl
+        //...req.body (tous les éléments de la requête seront copiés)
     })
     try {
         await data.save();
@@ -18,24 +21,56 @@ router.post('/post', async (req, res) => {
     }
 })
 
-//Get all Method
-router.get('/getAll', (req, res) => {
-    res.send('Get All API')
+//Get all Meubles Method
+router.get('/getAllMeubles', async (req, res) => {
+    try{
+        const data = await Meuble.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
 })
 
 //Get by ID Method
-router.get('/getOne/:id', (req, res) => {
-    res.send('Get by ID API')
+router.get('/getOneMeuble/:id', async (req, res) => {
+    try{
+        const data = await Meuble.findById(req.params.id);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
 })
 
 //Update by ID Method
-router.patch('/update/:id', (req, res) => {
-    res.send('Update by ID API')
+router.patch('/updateMeuble/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Meuble.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
 //Delete by ID Method
-router.delete('/delete/:id', (req, res) => {
-    res.send('Delete by ID API')
+router.delete('/deleteMeuble/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Meuble.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
 module.exports = router;
