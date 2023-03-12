e5fea2d6cf06a9ffe7e1d90e7df455124144f1b4
unknown <seanpnn00@gmail.com>
2023-03-12 23:47:44 +0100

    end of inspirations sections + styles

--- frontend/dist/index.c072103e.js
+++ frontend/dist/index.c072103e.js
@@ -778,6 +778,23 @@ img.addEventListener("click", function() {
         document.body.removeChild(this);
     });
 });
+// Gestion de la vitesse du scroll lors du clic sur "en savoir plus"
+var lien = document.querySelector('a[href="#savoir_plus"]');
+var destination = document.querySelector("#savoir_plus");
+function descendreEnDoucheur(destination) {
+    // Calcul de la position de l'élément de destination
+    var position = destination.offsetTop;
+    // Animation de la descente en douceur
+    window.scrollTo({
+        top: position,
+        behavior: "smooth"
+    });
+}
+// Ajout d'un événement sur le clic du lien
+lien.addEventListener("click", function(e) {
+    e.preventDefault();
+    descendreEnDoucheur(destination); // Appel de la fonction d'animation de descente en douceur
+});
 
 },{"@fortawesome/fontawesome-free/css/all.css":"8OILE","../css/styles.scss":"43exz","../js/tailwind.generated.js":"a6R8c","flowbite":"eakbs","../components/button":"le2UX","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"8OILE":[function() {},{}],"43exz":[function() {},{}],"a6R8c":[function(require,module,exports) {
 
--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -275,3 +275,27 @@ img.addEventListener('click', function() {
     document.body.removeChild(this);
   });
 });
+
+
+
+// Gestion de la vitesse du scroll lors du clic sur "en savoir plus"
+var lien = document.querySelector('a[href="#savoir_plus"]');
+var destination = document.querySelector('#savoir_plus');
+
+
+function descendreEnDoucheur(destination) {
+  // Calcul de la position de l'élément de destination
+  var position = destination.offsetTop;
+
+  // Animation de la descente en douceur
+  window.scrollTo({
+    top: position,
+    behavior: 'smooth'
+  });
+}
+
+// Ajout d'un événement sur le clic du lien
+lien.addEventListener('click', function(e) {
+  e.preventDefault(); 
+  descendreEnDoucheur(destination); // Appel de la fonction d'animation de descente en douceur
+});
