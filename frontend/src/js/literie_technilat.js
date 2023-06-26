c9b91e7ec8e54082755760deab5b7e7dfd867c69
unknown <seanpnn00@gmail.com>
2023-06-26 17:54:17 +0200

    finish add literie technilat items

---
+++ frontend/src/js/literie_technilat.js
@@ -0,0 +1,120 @@
+import litcof1 from "../img/literie/technilat/lit_coffre/lit_coffre-1_1.webp";
+
+import matatmo1 from "../img/literie/technilat/matelas_collection_atmosphere/berlin-atmosphere-ensemble-technilat.webp";
+import matatmo2 from "../img/literie/technilat/matelas_collection_atmosphere/deauville-atmosphere-1-technilat.webp";
+import matatmo3 from "../img/literie/technilat/matelas_collection_atmosphere/new-york-atmosphere-ensemble-technilat.webp";
+import matatmo4 from "../img/literie/technilat/matelas_collection_atmosphere/toronto-atmosphere-ensemble-technilat.webp";
+import matatmo5 from "../img/literie/technilat/matelas_collection_atmosphere/venise-atmosphere-ensemble-technilat.webp";
+
+import matgen1 from "../img/literie/technilat/matelas_collection_generale/athena-technilat.webp";
+import matgen2 from "../img/literie/technilat/matelas_collection_generale/azur-technilat.webp";
+import matgen3 from "../img/literie/technilat/matelas_collection_generale/blues-technilat.webp";
+import matgen4 from "../img/literie/technilat/matelas_collection_generale/bora-technilat.webp";
+import matgen5 from "../img/literie/technilat/matelas_collection_generale/calypso.webp";
+import matgen6 from "../img/literie/technilat/matelas_collection_generale/coronis-technilat.webp";
+import matgen7 from "../img/literie/technilat/matelas_collection_generale/cosmos-technilat_1.webp";
+import matgen8 from "../img/literie/technilat/matelas_collection_generale/cybele.webp";
+import matgen9 from "../img/literie/technilat/matelas_collection_generale/feuille-technilat.webp";
+import matgen10 from "../img/literie/technilat/matelas_collection_generale/hera.webp";
+import matgen11 from "../img/literie/technilat/matelas_collection_generale/hypnos-technilat_1.webp";
+import matgen12 from "../img/literie/technilat/matelas_collection_generale/lotus-technilat.webp";
+import matgen13 from "../img/literie/technilat/matelas_collection_generale/lys-technilat.webp";
+import matgen14 from "../img/literie/technilat/matelas_collection_generale/madisson-technilat.webp";
+import matgen15 from "../img/literie/technilat/matelas_collection_generale/nectar-technilat.webp";
+import matgen16 from "../img/literie/technilat/matelas_collection_generale/orphee.webp";
+import matgen17 from "../img/literie/technilat/matelas_collection_generale/ponant-technilat.webp";
+import matgen18 from "../img/literie/technilat/matelas_collection_generale/rhodes.webp";
+import matgen19 from "../img/literie/technilat/matelas_collection_generale/simoun-technilat.webp";
+import matgen20 from "../img/literie/technilat/matelas_collection_generale/titan.webp";
+
+import mathot1 from "../img/literie/technilat/sommier_collection_hotel/charme-hotel-technilat.webp";
+import mathot2 from "../img/literie/technilat/sommier_collection_hotel/palace-hotel-technilat.webp";
+import mathot3 from "../img/literie/technilat/sommier_collection_hotel/suite-hotel-technilat.webp";
+
+import matmem1 from "../img/literie/technilat/matelas_collection_memonat/alpha.webp";
+import matmem2 from "../img/literie/technilat/matelas_collection_memonat/astra.webp";
+import matmem3 from "../img/literie/technilat/matelas_collection_memonat/cocoona_1.webp";
+import matmem4 from "../img/literie/technilat/matelas_collection_memonat/reva-technilat.webp";
+import matmem5 from "../img/literie/technilat/matelas_collection_memonat/topper-memonat_2.webp";
+
+import matspyr1 from "../img/literie/technilat/matelas_collection_spyral/artic-technilat.webp";
+import matspyr2 from "../img/literie/technilat/matelas_collection_spyral/declic-technilat.webp";
+import matspyr3 from "../img/literie/technilat/matelas_collection_spyral/dynamic-technilat.webp";
+import matspyr4 from "../img/literie/technilat/matelas_collection_spyral/energic-technilat.webp";
+import matspyr5 from "../img/literie/technilat/matelas_collection_spyral/ergonomic-technilat.webp";
+import matspyr6 from "../img/literie/technilat/matelas_collection_spyral/esthetic-technilat.webp";
+import matspyr7 from "../img/literie/technilat/matelas_collection_spyral/fantastique-technilat.webp";
+import matspyr8 from "../img/literie/technilat/matelas_collection_spyral/idyllic_1.webp";
+import matspyr9 from "../img/literie/technilat/matelas_collection_spyral/magnific-technilat.webp";
+import matspyr10 from "../img/literie/technilat/matelas_collection_spyral/mystic-technilat.webp";
+import matspyr11 from "../img/literie/technilat/matelas_collection_spyral/oniric-technilat.webp";
+import matspyr12 from "../img/literie/technilat/matelas_collection_spyral/poetic-technilat.webp";
+import matspyr13 from "../img/literie/technilat/matelas_collection_spyral/tonic-technilat.webp";
+import matspyr14 from "../img/literie/technilat/matelas_collection_spyral/unic-technilat.webp";
+
+import matsync1 from "../img/literie/technilat/matelas_collection_synchroflex/emeraude.webp";
+import matsync2 from "../img/literie/technilat/matelas_collection_synchroflex/perle-fixe-technilat.webp";
+import matsync3 from "../img/literie/technilat/matelas_collection_synchroflex/rubis-fixe-technilat.webp";
+
+import mataubr1 from "../img/literie/technilat/matelas_collection_terres_daubrac/alba-terres-d-aubrac-technilat.webp";
+import mataubr2 from "../img/literie/technilat/matelas_collection_terres_daubrac/alto-braco-terres-d-aubrac-technilat.webp";
+import mataubr3 from "../img/literie/technilat/matelas_collection_terres_daubrac/artiga-terres-d-aubrac-technilat.webp";
+import mataubr4 from "../img/literie/technilat/matelas_collection_terres_daubrac/aura-ensemble.webp";
+import mataubr5 from "../img/literie/technilat/matelas_collection_terres_daubrac/l-aubrac-terres-d-aubrac-technilat-1.webp";
+import mataubr6 from "../img/literie/technilat/matelas_collection_terres_daubrac/tpr-terres-d-aubrac-technilat.webp";
+
+import ore1 from "../img/literie/technilat/oreiller/oreiller-natura.webp";
+
+import pieli1 from "../img/literie/technilat/pieds_lit/chene-thumb.webp";
+import pieli2 from "../img/literie/technilat/pieds_lit/ori-pieds-de-lit-technilat.webp";
+import pieli3 from "../img/literie/technilat/pieds_lit/pieds-4-socles_1.webp";
+import pieli4 from "../img/literie/technilat/pieds_lit/pieds-classique.webp";
+import pieli5 from "../img/literie/technilat/pieds_lit/pieds-synchro.webp";
+import pieli6 from "../img/literie/technilat/pieds_lit/st3403.webp";
+
+import somfi1 from "../img/literie/technilat/sommier_fixe/concept-sommier-technilat.webp";
+import somfi2 from "../img/literie/technilat/sommier_fixe/sommier-fixe-decorec.webp";
+import somfi3 from "../img/literie/technilat/sommier_fixe/sommier-fixe-decorev_4.webp";
+import somfi4 from "../img/literie/technilat/sommier_fixe/sommier-fixe-extraplat.webp";
+import somfi5 from "../img/literie/technilat/sommier_fixe/sommier-fixe-somplots.webp";
+import somfi6 from "../img/literie/technilat/sommier_fixe/synchroflex-fixe-metal.webp";
+import somfi7 from "../img/literie/technilat/sommier_fixe/vintage-rond-technilat.webp";
+
+import somrel1 from "../img/literie/technilat/sommier_relevable/boxdeco-technilat-3.webp";
+import somrel2 from "../img/literie/technilat/sommier_relevable/combilat-vline-technilat.webp";
+import somrel3 from "../img/literie/technilat/sommier_relevable/duoplot-vline-technilat.webp";
+import somrel4 from "../img/literie/technilat/sommier_relevable/fuerte-vline-technilat.webp";
+import somrel5 from "../img/literie/technilat/sommier_relevable/mono-box-technilat-3.webp";
+import somrel6 from "../img/literie/technilat/sommier_relevable/mono-boxdeco-1-technilat.webp";
+import somrel7 from "../img/literie/technilat/sommier_relevable/neolat-vline-technilat.webp";
+import somrel8 from "../img/literie/technilat/sommier_relevable/sommier-relevable-mono-lattes.webp";
+import somrel9 from "../img/literie/technilat/sommier_relevable/sommier-relevable-mono-plots.webp";
+
+import tabchev1 from "../img/literie/technilat/tables_chevet/chevet-design-technilat.webp";
+
+import tetelit1 from "../img/literie/technilat/tete_lit/alaska-tete-de-lit-technilat.webp";
+import tetelit2 from "../img/literie/technilat/tete_lit/bali-1-tete-de-lit-technilat.webp";
+import tetelit3 from "../img/literie/technilat/tete_lit/banquise-2-tete-de-lit-technilat.webp";
+import tetelit4 from "../img/literie/technilat/tete_lit/bermudes-1-tete-de-lit-technilat.webp";
+import tetelit5 from "../img/literie/technilat/tete_lit/borneo-tete-de-lit-technilat.webp";
+import tetelit6 from "../img/literie/technilat/tete_lit/boudoir-tete-de-lit-technilat.webp";
+import tetelit7 from "../img/literie/technilat/tete_lit/couette-tete-de-lit-technilat.webp";
+import tetelit8 from "../img/literie/technilat/tete_lit/diagonale-tete-de-lit-technilat.webp";
+import tetelit9 from "../img/literie/technilat/tete_lit/fanon-tete-de-lit-technilat.webp";
+import tetelit10 from "../img/literie/technilat/tete_lit/java-tete-de-lit-technilat.webp";
+import tetelit11 from "../img/literie/technilat/tete_lit/maldives-tete-de-lit-technilat.webp";
+import tetelit12 from "../img/literie/technilat/tete_lit/noeuds-tete-de-lit-technilat.webp";
+import tetelit13 from "../img/literie/technilat/tete_lit/panoramique-1-tete-de-lit-technilat.webp";
+import tetelit14 from "../img/literie/technilat/tete_lit/piana-2-pans-pearl-grey-tete-de-lit-technilat.webp";
+import tetelit15 from "../img/literie/technilat/tete_lit/piana-tete-de-lit.webp";
+import tetelit16 from "../img/literie/technilat/tete_lit/sumatra-tete-de-lit-technilat.webp";
+import tetelit17 from "../img/literie/technilat/tete_lit/tendance-tete-de-lit-technilat.webp";
+import tetelit18 from "../img/literie/technilat/tete_lit/tete-de-lit-lisse-2-pans.webp";
+import tetelit19 from "../img/literie/technilat/tete_lit/tete-de-lit-lisse-3-pans.webp";
+import tetelit20 from "../img/literie/technilat/tete_lit/thevenon-1-tete-de-lit-technilat.webp";
+
+
+
+
+
+
